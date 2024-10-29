ed608c0e1c21bf4c0a63d20dd49aefd9560d074a
vanshikaag13 <agrawalvanshi286@gmail.com>
2024-10-29 22:59:36 +0530

    added replica db record

--- route53.tf
+++ route53.tf
@@ -4,8 +4,17 @@ resource "aws_route53_zone" "main" {
 
 resource "aws_route53_record" "primary_record" {
   zone_id = aws_route53_zone.main.zone_id
-  name     = "primary-db.multi-db-orchestrator.com" # Change to your domain
+  name     = "primary-db.multi-db-orchestrator.com" 
   type     = "CNAME"
   ttl      = "60"
   records   = [aws_db_instance.primary.address]
+}   
+
+resource "aws_route53_record" "replica_record" {
+  provider = aws.replica
+  zone_id  = aws_route53_zone.main.zone_id
+  name      = "replica-db.multi-db-orchestrator.com" 
+  type      = "CNAME"
+  ttl       = "60"
+  records   = [aws_db_instance.replica.address]
 }
