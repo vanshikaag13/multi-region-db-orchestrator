059331afb7aa99b16c3cce7ddff0e9587cd3edf1
vanshikaag13 <agrawalvanshi286@gmail.com>
2024-11-01 21:27:36 +0530

    added sns subscription

--- sns.tf
+++ sns.tf
@@ -6,3 +6,9 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "myemail@gmail.com"  
 }
+resource "aws_sns_topic_subscription" "lambda_subscription" {
+  topic_arn = aws_sns_topic.notification_topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.failover_lambda.arn  
+  depends_on = [aws_lambda_permission.allow_sns]
+}
